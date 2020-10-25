00e040846e733ec1ffcdf9197c63f33405a03544
Kazeem Quadri <33027659+Darkace01@users.noreply.github.com>
2020-10-25 12:24:40 +0100

    Adding AutoMapper for mapping our DTO

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,8 +33,8 @@ namespace WebApiCore3
           ));
             services.AddControllers();
 
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             //Dependency Injection
-            // services.AddScoped<IWebApiRepo, MockWebApiRepo>();
             services.AddScoped<IWebApiRepo, SqlWebApiCoreRepo>();
         }
 
